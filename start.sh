a6e0a3e13633186c49d9513ee990a17bb0555b31
Alexandr Samuilov <alexandr.s@deovr.com>
2025-03-02 22:33:33 +0200

    2-3-2025

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 echo "run db migration"
-export $(grep -v '^#' /app/app.env | xargs)
+source /app/app.env
 /app/migrate -path /app/migration -database "$DB_SOURCE" -verbose up
 echo "start the app"
 exec "$@"
